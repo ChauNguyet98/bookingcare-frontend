c30f0df7461e25e209e0cce8da319cf55080b3a8
Nguyet Chau <chau.thi.nguyet@styl.solutions>
2023-04-01 20:52:53 +0700

    update login ui

--- src/app/pages/auth/login/login.tsx
+++ src/app/pages/auth/login/login.tsx
@@ -1,34 +1,85 @@
-import React, { Component } from "react";
-import Form from "react-bootstrap/Form";
+import { Component } from "react";
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import { BsPersonCircle } from "react-icons/bs";
 
 class Login extends Component {
   constructor(props: any) {
     super(props);
   }
 
-  render() {
-    return (
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
+  state = {
+    userName: "",
+    password: "",
+  };
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+  onUserNameChange = (event: any) => {
+    this.setState({
+      userName: event.target.value,
+    });
+  };
+
+  onPasswordChange = (event: any) => {
+    this.setState({
+      password: event.target.value,
+    });
+  };
+
+  submitForm = () => {
+    console.log(this.state);
+  };
+
+  render() {
+    const { userName, password } = this.state;
+
+    return (
+      <div className="login">
+        <Form>
+          <div className="login__title">Login</div>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label className="login__label">Email address</Form.Label>
+            <InputGroup className="mb-3">
+              <InputGroup.Text id="basic-addon1">
+                <BsPersonCircle />
+              </InputGroup.Text>
+              <Form.Control
+                type="text"
+                placeholder="UserName"
+                value={userName}
+                onChange={($event) => this.onUserNameChange($event)}
+              />
+            </InputGroup>
+            {/* <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text> */}
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label className="login__label">Password</Form.Label>
+            <InputGroup className="mb-3">
+              <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={($event) => this.onPasswordChange($event)}
+              />
+            </InputGroup>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Remember Me" />
+          </Form.Group>
+          <Button
+            type="submit"
+            className="login__btn"
+            onClick={() => this.submitForm()}
+          >
+            Login
+          </Button>
+        </Form>
+      </div>
     );
   }
 }
